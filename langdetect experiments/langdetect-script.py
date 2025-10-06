526d6522edb5e016e170548e337d2bc8b1f59267
Mark A <andradem@tcd.ie>
2025-10-06 16:37:34 +0100

    Added sample datasets to langdetect

--- langdetect experiments/langdetect-script.py
+++ langdetect experiments/langdetect-script.py
@@ -98,7 +98,7 @@ class LanguageDetector:
         return
 
 if __name__ == "__main__":
-    folder_path = "langdetect experiments/c4"
+    folder_path = "langdetect experiments/100samples"
     for file_name in os.listdir(folder_path):
         full_path = os.path.join(folder_path, file_name)
         if os.path.isfile(full_path):
