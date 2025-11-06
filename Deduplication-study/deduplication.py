cefb6ceaff32302b60f4e5011be1d78918c710df
andradeM17 <andradem@tcd.ie>
2025-11-06 12:51:51 +0000

    Checks for triplets

--- Deduplication-study/deduplication.py
+++ Deduplication-study/deduplication.py
@@ -1,17 +1,34 @@
 from collections import Counter
 
-datasets = ["EUbookshop", "Wikipedia", "XLEnt"]
-
-for d in datasets:
-    filename = f"Deduplication-study/{d}.txt"
-
-    with open(filename, "r", encoding="utf-8") as f:
-        lines = [line.strip() for line in f if line.strip()]  # remove empty lines & whitespace
-
-    # Count each line’s frequency
+def find_duplicates(lines):
     line_counts = Counter(lines)
-
-    # Find duplicates
     duplicates = {line: count for line, count in line_counts.items() if count > 1}
+    return duplicates
 
-    print(f"Total completely duplicate lines in {d}: {len(duplicates)}\n")
+def find_duplicate_triplets(lines):
+    triplets = [
+        tuple(lines[i:i+3]) for i in range(len(lines) - 2)
+    ]
+    counts = Counter(triplets)
+    repeated = {triplet: count for triplet, count in counts.items() if count > 1}
+    return repeated
+
+
+def main():
+    datasets = ["EUbookshop", "Wikipedia", "XLEnt"]
+
+    for d in datasets:
+        filename = f"Deduplication-study/{d}.txt"
+
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = [line.strip() for line in f if line.strip()]  # remove empty lines & whitespace
+
+            # Count each line’s frequency
+            duplicates= find_duplicates(lines)
+            print(f"Total completely duplicate lines in {d}: {len(duplicates)}.")
+
+            three_in_a_row_duplicates = find_duplicate_triplets(lines)
+            print(f"Found {len(three_in_a_row_duplicates)} sets of duplicate triplets of lines in {d}.\n")
+
+if __name__ == "__main__":
+    main()
